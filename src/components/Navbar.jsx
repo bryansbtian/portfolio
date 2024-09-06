112397e271c1268e2d2427f10f327678f9e6ddcb
bryansbtian <bryan.sebastian189@gmail.com>
2024-09-06 15:07:06 -0400

    added new things

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import logo from '../assets/logo.jpeg'
+import { FaGithub } from 'react-icons/fa';
 import { FaLinkedin } from 'react-icons/fa';
 import { FaInstagram } from 'react-icons/fa';
+
 
 const Navbar = () => {
   return (
@@ -9,6 +11,7 @@ const Navbar = () => {
             <img className='mx-2 w-10' src={logo} alt="" />
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+            <a href='https://github.com/bryansbtian?tab=repositories'><FaGithub /></a>
             <a href='https://www.linkedin.com/in/bry189/'><FaLinkedin /></a>
             <a href='https://www.instagram.com/bryansbtian/'><FaInstagram /></a>
         </div>
