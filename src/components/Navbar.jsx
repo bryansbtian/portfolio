dd727673698a3b1dd901c5e9a6ddd81cd06552bf
bryansbtian <bryan.sebastian189@gmail.com>
2026-06-17 08:51:08 -0500

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,25 @@
-import logo from '../assets/logo.jpeg'
-import { FaGithub } from 'react-icons/fa';
-import { FaLinkedin } from 'react-icons/fa';
-import { FaInstagram } from 'react-icons/fa';
-
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav className="mb-20 flex items-center justify-between py-6">
-        <div className="flex flex-shrink-0 items-center">
-            <a href="/"><img className='mx-2 w-10' src={logo} alt="" /></a>
-        </div>
-        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-            <a href='https://github.com/bryansbtian?tab=repositories'><FaGithub /></a>
-            <a href='https://www.linkedin.com/in/bry189/'><FaLinkedin /></a>
-            <a href='https://www.instagram.com/bryansbtian/'><FaInstagram /></a>
-        </div>
+    <nav className="mb-15 flex items-center justify-end py-6">
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://github.com/bryansbtian?tab=repositories">
+          <FaGithub />
+        </a>
+
+        <a href="https://www.linkedin.com/in/bry189/">
+          <FaLinkedin />
+        </a>
+
+        <a href="https://www.instagram.com/bryansbtian/">
+          <FaInstagram />
+        </a>
+      </div>
     </nav>
   );
 };
 
-export default Navbar
+export default Navbar;
