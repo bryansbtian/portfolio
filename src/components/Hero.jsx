90e56c2932abe02ea325315da4f7ab2490391563
Bryan Sebastian Susanto <bryan.sebastian189@gmail.com>
2026-08-17 19:42:14 -0400

    fix alignment

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/bryanProfile.jpeg";
+import profilePic from "../assets/bryanProfile.jpg";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
